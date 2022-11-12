aec69c5782be6577b0e234c1bbf062bc633eb380
Agus Rivera <agustinriveratapi3@gmail.com>
2022-11-12 20:59:33 -0300

    agregamos formularios

--- proyrcto/appcoder/urls.py
+++ proyrcto/appcoder/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from appcoder.views import *
 
-urlpatterns = [
+urlpatterns =[
     path("estudiantes/", estudiantes, name= "coder-estudiantes"),
     path("profesores/", profesores, name= "coder-profesores"),
+    path("profesores/crear/", creacion_profesores, name= "coder-profesores-crear"),
     path("cursos/", cursos, name= "coder-cursos"),
+    path("cursos/crear/", creacion_curso, name="coder-cursos-crear"),
     path("entregables/", entregables, name= "coder-entregables"),
     path("inicio/", inicio, name= "coder-inicio"),
 ]
